f0ee1462ecf4aa513e42ea8273c82b952393ce75
Dennis Zhang <ndenniszhang@gmail.com>
2025-05-16 16:08:26 -0400

    success deploy update wrangler

--- open-next.config.ts
+++ open-next.config.ts
@@ -5,3 +5,13 @@ import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cac
 export default defineCloudflareConfig({
   incrementalCache: r2IncrementalCache,
 });
+
+
+// import { withRegionalCache } from "@opennextjs/cloudflare/overrides/incremental-cache/regional-cache";
+
+// export default defineCloudflareConfig({
+//   incrementalCache: withRegionalCache(r2IncrementalCache, {
+//     mode: "long-lived", // or "short-lived"
+//     shouldLazilyUpdateOnCacheHit: true,
+//   }),
+// });
